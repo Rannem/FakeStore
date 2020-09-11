464cf6564bb1553948772a01a7f4711e4e1c14e0
SindreBroholm <sindreset@gmail.com>
2020-09-11 14:50:06 +0200

    la til mye var stresset 2.3.4

--- src/main/java/no/fakestore/fakestore/GameLibrary.java
+++ src/main/java/no/fakestore/fakestore/GameLibrary.java
@@ -12,7 +12,7 @@ public class GameLibrary {
 
     public GameLibrary() {
         games = new ArrayList<>();
-        games.add(new Game("49.99", 10003, "Minecraft", "Mojang AB", "PC, Console", GameGenre.SANDBOX, "Amazing sandbox game with zombies.", "November 19, 2009", "Suitable for 7+", "/Assets/minecraft.jpg"));
+        games.add(new Game("49.99", 10103, "Minecraft", "Mojang AB", "PC, Console", GameGenre.SANDBOX, "Amazing sandbox game with zombies.", "November 19, 2009", "Suitable for 7+", "/Assets/minecraft.jpg"));
     }
 
     public List<Game> getGames() {
--- src/main/java/no/fakestore/fakestore/MovieLibrary.java
+++ src/main/java/no/fakestore/fakestore/MovieLibrary.java
@@ -12,7 +12,7 @@ public class MovieLibrary {
 
     public MovieLibrary() {
         movies = new ArrayList<>();
-        movies.add(new Movie("20.00", 10002, "Tenet ", "Christopher Nolan", "Christopher Nolan", MovieGenre.ACTION, "2h 30min", "John David Washington, Robert Pattinson, Elizabeth Debicki", "Amazing movie, starring amazing actors.", "\n" +
+        movies.add(new Movie("20.00", 10012, "Tenet ", "Christopher Nolan", "Christopher Nolan", MovieGenre.ACTION, "2h 30min", "John David Washington, Robert Pattinson, Elizabeth Debicki", "Amazing movie, starring amazing actors.", "\n" +
                 "August 28, 2020", "12 years and above", "/Assets/tenet.jpg"));
     }
 
