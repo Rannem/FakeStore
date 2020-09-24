7676f1cb17f279d8a14f2678708b53461d9ea3b1
SindreBroholm <sindreset@gmail.com>
2020-09-24 13:31:37 +0200

    pom update

--- src/main/java/no/fakestore/fakestore/User.java
+++ src/main/java/no/fakestore/fakestore/User.java
@@ -1,10 +1,19 @@
 package no.fakestore.fakestore;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String firstName;
+
+    @Size(min = 1, max = 30, message = "Firstname must be between 1 and 30 characters")
     private String lastName;
     private Gender gender = Gender.OTHER;
     private String eMail;
